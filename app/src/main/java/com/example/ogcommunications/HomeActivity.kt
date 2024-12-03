5067ffc47c4d193ed65a14ce2786e9173d2bd959
vishalprasad1912 <vishalprasad7851@gmail.com>
2024-12-03 13:52:50 +0530

    Made Splash Screen and Home page

---
+++ app/src/main/java/com/example/ogcommunications/HomeActivity.kt
@@ -0,0 +1,41 @@
+package com.example.ogcommunications
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonHomeCall = findViewById<Button>(R.id.btnHomeCall)
+
+        buttonHomeCall.setOnClickListener{
+            val phoneNo = "tel:+917291006049"
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse(phoneNo)
+
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1) }
+            else { startActivity(callIntent) }
+        }
+    }
+
+}
